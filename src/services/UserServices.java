d3467267d74af985a40219fdefa121ee0de33000
aymenzaabar <aymen.zaabar@esprit.tn>
2021-10-16 01:39:28 +0100

    crud1

--- src/services/UserServices.java
+++ src/services/UserServices.java
@@ -79,4 +79,68 @@ public class UserServices {
     return  users;
     
 }
+         public User  retrieveUser(int userId){
+        User u = new User();
+        String req = "SELECT * from user WHERE user.userId="+userId;
+            try {
+            ste = cnx.prepareStatement(req);
+            ResultSet rs = ste.executeQuery();
+            while (rs.next()){
+                 u.setUserId(rs.getInt(1));
+                 u.setName(rs.getString("name"));
+                  u.setPassword(rs.getString("password"));
+                 u.setCity(rs.getString("city"));
+                 u.setGouvernorat(rs.getString("gouvernorat"));
+                  u.setPhone(rs.getString("phone"));
+                 u.setMail(rs.getString("mail"));
+                 u.setRole(rs.getString("role"));
+                  u.setMontant_donne(rs.getFloat("montant_donne"));
+                
+            }
+            System.out.println("retrieve user success");
+            
+        } catch (SQLException ex) {
+                System.out.println("Erreur retrieve user");
+        }
+        return u;
+    }
+         
+           public void deleteUser(int userId){
+        
+            //String req = "DELETE FROM user WHERE (userId="+userId+")";
+            String req = "DELETE FROM user WHERE user.userId="+userId;
+            try {
+            ste = cnx.prepareStatement(req);
+            ste.executeUpdate();
+            System.out.println("User deleted");
+        } catch (SQLException ex) {
+                System.out.println("Erreur delete user");
+        }
+    }
+         
+          public void updateUser(int userId, User user1){
+        
+        //String req = "UPDATE user SET WHERE user.userId="+userId;
+        String req = "UPDATE user SET name=?,password=?,city=?,gouvernorat=?,phone=?,mail=?,role=?,montant_donne=? WHERE user.userId=" + userId;
+        
+        try{
+                        ste = cnx.prepareStatement(req);
+           ste.setString(1, user1.getName());
+            ste.setString(2, user1.getPassword());
+            ste.setString(3, user1.getCity());
+            ste.setString(4, user1.getGouvernorat());
+            ste.setString(5, user1.getPhone());
+            ste.setString(6, user1.getMail());
+            ste.setString(7, user1.getRole());
+            ste.setFloat(8, user1.getMontant_donne());
+            ste.executeUpdate();
+            System.out.println("Event updated");
+            
+        }
+        catch(SQLException ex){
+            System.out.println("Erreur update event");
+            
+        }
+        
+    }
 }
--- src/tests/UserTest.java
+++ src/tests/UserTest.java
@@ -18,8 +18,12 @@ public class UserTest {
         UserServices use = new UserServices();
         User u1 = new User("Aymen","97262697","Sahloul","Sousse","93977499","aymen.zaabar@espri.tn","admin",1200);
         User u2=new User("Hassen","123456789","Khzema","Sousse","19819800","hassen.hochlef@espri.tn","admin",500.5f);
-        //use.addUser(u2);
-       // System.out.println(use.retrieveallUser()); 
+         User u3=new User("samir","25644595","mansoura","Sousse","19819800","samir.pessiron@espri.tn","admin",500.5f);
 
+       // use.addUser(u3);
+       // System.out.println(use.retrieveallUser()); 
+      //System.out.println(use.retrieveUser(1));
+      //use.deleteUser(3);
+      //use.updateUser(2,u3);
     }
 }
